8b6e054f38cbc33ed7ac0a11cabd75e90a340e35
Polumetis <polumetis@protonmail.com>
2018-03-20 03:09:15 -0500

    add controls

--- src/components/States/Init/index.js
+++ src/components/States/Init/index.js
@@ -23,7 +23,45 @@ export function Init() {
 
           {/* Help */}
           <div class={ style.help }>
-            Press space to continue
+            <div class={ style.help_text }>Press Space to continue.</div>
+
+            <div class={ style.help_controls }>
+              <span class={ style.help_controls_label }>
+                Fire
+              </span>
+              &nbsp;- Space<br/>
+
+              <span class={ style.help_controls_label }>
+                Boost
+              </span>
+              &nbsp;- Left shift<br/>
+
+              <span class={ style.help_controls_label }>
+                Forward / reverse thrust
+              </span>
+              &nbsp;- W, Up Arrow / S, Down Arrow<br/>
+
+              <span class={ style.help_controls_label }>
+                Rotational thrust
+              </span>
+              &nbsp;- A, Left Arrow / D, Right Arrow<br/>
+
+              <span class={ style.help_controls_label }>
+                Lateral thrust
+              </span>
+              &nbsp;- Q, Left Bracket / E, Right Bracket<br/>
+
+              <span class={ style.help_controls_label }>
+                Pause
+              </span>
+              &nbsp;- Escape<br/>
+
+              <span class={ style.help_controls_label }>
+                Resume
+              </span>
+              &nbsp;- Enter
+            </div>
+
           </div>
 
           {/* Corners */}
